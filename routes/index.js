78e43dcb24daa6ad2f44c38e14979508239116cf
yangttlxl@sina.com <yangttlxl@sina.com>
2017-05-08 15:41:23 +0800

    lxlhumkoa2展示如何做个koa

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var router = require('koa-router')();
+
+router.get('/', async function (ctx, next) {
+  ctx.state = {
+    title: 'koa2 title'
+  };
+
+  await ctx.render('index', {
+  });
+});
+
+router.get('/op', async function (ctx, next) {
+    ctx.state = {
+        title: 'op'
+    };
+
+    await ctx.render('index', {
+    });
+});
+
+
+module.exports = router;
